abde345d013941dfb5f9d90bdcbffb22ee7da760
Arian <arian.kulmer@web.de>
2026-02-16 06:04:46 +0000

    giuliac: Import QTI vibrator effect and rename
    Change-Id: I60e4623e5d3fc5c5582e70c7f751c0928f77563d

--- device.mk
+++ device.mk
@@ -80,6 +80,7 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.vibrator.service
 
 $(call soong_config_set_bool,qti_vibrator,use_effect_stream,true)
+$(call soong_config_set,qti_vibrator,effect_lib,libqtivibratoreffect.oplus.giuliac)
 
 # WiFi firmware symlinks
 PRODUCT_PACKAGES += \
