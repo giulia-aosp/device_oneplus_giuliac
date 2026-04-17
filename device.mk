b0796a4732ce85118ee26b8da52ac42d6e66dc6d
kreitsir0 <kreitsir@gmail.com>
2026-04-17 18:21:38 +0300

    giuliac: fixup pixelworks color calibration on oss hwc

--- device.mk
+++ device.mk
@@ -38,7 +38,6 @@ PRODUCT_COPY_FILES += \
 $(call soong_config_set,qtidisplay,bitclk_rate_permissive,true)
 $(call soong_config_set,surfaceflinger,frame_rate_category_high,120)
 $(call soong_config_set,qtidisplay,sm8650_pxlw_vendor_namespace,vendor/oneplus/giuliac)
-$(call soong_config_set,qtidisplay,sm8650_pxlw_hw_iris7,true)
 
 # HIDL
 PRODUCT_PACKAGES += \
